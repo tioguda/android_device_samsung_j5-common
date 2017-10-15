2ee4cb4e3390ff2a53c502c6aa3517a51ad65f9f
tioguda <guda.flavio@gmail.com>
2017-10-15 07:49:39 -0200

    Remove KernelAdiutor, Adaway and OTAUpdates

--- product/otaupdates.mk
+++
@@ -1,3 +0,0 @@
-# OTAUpdates
-PRODUCT_PACKAGES += \
-    OTAUpdates 
--- product/tesla.mk
+++
@@ -1,7 +0,0 @@
-# KernelAdiutor
-PRODUCT_PACKAGES += \
-    KernelAdiutor
-
-# Adaway
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/common/app/adaway.apk:system/app/adaway.apk
