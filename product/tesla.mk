096c158c66e075cd7000ccc22244b2d52d5ff7f1
tioguda <guda.flavio@gmail.com>
2017-10-15 07:07:56 -0200

    Remove Magisk

--- product/tesla.mk
+++ product/tesla.mk
@@ -1,10 +1,3 @@
-# Default root Method := supersu/magisk
-DEFAULT_ROOT_METHOD := magisk
-
-# Magisk
-PRODUCT_COPY_FILES += \
-    vendor/tesla/prebuilt/zip/magisk.zip:system/addon.d/magisk.zip
-
 # KernelAdiutor
 PRODUCT_PACKAGES += \
     KernelAdiutor
